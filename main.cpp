d846ddb203f0ab9e5bee65f0bceebe88577503ee
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-10-02 16:13:49 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
-int Recursive(int n) {
-	if (n <= 1) { return 1; }
+int RecursiveWage(int n,int wage) {
+	if (n <= 1) {
+		return wage;
+	}
 
-
+	return (wage + RecursiveWage(n - 1, wage * 2 - 50));
 }
 
 int main() {
+
+	int workTime = 8;
+	int normalHourlyWage = 1072;
+	int recursiveHourlyWage = 100;
+
+	printf("Ä‹A“I‚È’À‹à‘ÌŒn  ŽžŠÔ:%d ‹‹—¿:%d\n", workTime,RecursiveWage(workTime, recursiveHourlyWage));
+	printf("ˆê”Ê“I‚È’À‹à‘ÌŒn  ŽžŠÔ:%d ‹‹—¿:%d\n", workTime, normalHourlyWage * workTime);
 
 
 	return 0;
