4590f0f34de0455529c6dac7148297f631d705dd
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-10-02 11:22:57 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+
+template <typename Type>
+Type Min(Type a, Type b) {
+	return static_cast<Type>(a < b ? a : b);
+}
+
+
+char Min(char a, char b) {
+	printf("数字以外は代入できません");
+	return '\n';
+}
 
 int main() {
 
+	printf("%d\n", Min(1, 5));
+	printf("%f\n", Min(1.0f, 2.0f));
+	printf("%lf\n", Min(1.0, 2.0));
+	printf("%c\n", Min('1', '5'));
 
 	return 0;
 }
