e15180cfbb1d7a5d0629e87f9d7454b5afad9f9d
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-10-14 18:13:43 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,51 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+#include <functional>
+
+//void SetTimeout(std::function<void()> func, int second) {
+//	Sleep(second * 1000);
+//	func();
+//}
 
 int main() {
 
+	srand(time(nullptr));
+	int selectNumber = 0;
+
+	std::function<void()> SelectRole = [&selectNumber]() {
+		while (true) {
+			if (selectNumber != 1 && selectNumber != 2) {
+				printf("サイコロの目が半(奇数)なら'1'を、丁(偶数)なら'2'を入力してください\n入力 : ");
+				scanf_s("%d", &selectNumber);
+			}
+			else {
+				printf("結果は...\n\n");
+				break;
+			}
+		}
+	};
+
+	std::function<void()> ResultRole = [&selectNumber]() {
+		int diceNum = rand() % 6 + 1;
+		printf("結果 : %d\n", diceNum);
+		if ((selectNumber == 1 && (diceNum % 2) == 1) || (selectNumber == 2 && (diceNum % 2) == 0)) {
+			printf("正解!!\nおめでとう!!\n");
+		}
+		else {
+			printf("不正解\n残念...\n");
+		}
+	};
+
+	auto SetTimeout = [](std::function<void()> func, int second) {
+		Sleep(second * 1000);
+		func();
+	};
+
+	printf("サイコロを振った！\n");
+
+	SetTimeout(SelectRole,1);
+	SetTimeout(ResultRole, 3);
 
 	return 0;
 }
