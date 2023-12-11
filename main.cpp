a3d6b40a1e71698d5c052228cf1f766d60bd4e79
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-12-11 12:06:00 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,71 @@
 #include <stdio.h>
+#include <list>
+
 
 int main() {
+
+	std::list<const char*> yamanoteLine = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	printf("1970\n");
+	for (std::list<const char*>::iterator itYamanoteLine = yamanoteLine.begin(); itYamanoteLine != yamanoteLine.end(); ++itYamanoteLine) {
+		printf("%s\n", *itYamanoteLine);
+	}
+
+	for (std::list<const char*>::iterator itYamanoteLine = yamanoteLine.begin(); itYamanoteLine != yamanoteLine.end(); ++itYamanoteLine) {
+		if (*itYamanoteLine == "Tabata") {
+			itYamanoteLine = yamanoteLine.insert(itYamanoteLine, "Nishi-Nippori");
+			++itYamanoteLine;
+		}
+	}
+
+	printf("\n");
+	printf("2019\n");
+	for (std::list<const char*>::iterator itYamanoteLine = yamanoteLine.begin(); itYamanoteLine != yamanoteLine.end(); ++itYamanoteLine) {
+		printf("%s\n", *itYamanoteLine);
+	}
+
+	for (std::list<const char*>::iterator itYamanoteLine = yamanoteLine.begin(); itYamanoteLine != yamanoteLine.end(); ++itYamanoteLine) {
+		if (*itYamanoteLine == "Tamachi") {
+			itYamanoteLine = yamanoteLine.insert(itYamanoteLine, "Takanawa Gateway");
+			++itYamanoteLine;
+		}
+	}
+
+	printf("\n");
+	printf("2022\n");
+	for (std::list<const char*>::iterator itYamanoteLine = yamanoteLine.begin(); itYamanoteLine != yamanoteLine.end(); ++itYamanoteLine) {
+		printf("%s\n", *itYamanoteLine);
+	}
+
 
 
 	return 0;
